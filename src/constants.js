e73e6b72233027c17f4a19a9bcb9198a855abb60
Dante0214 <rn_chic@naver.com>
2025-11-03 17:37:25 +0900

    fix: 수익률 출력 형태 수정

--- src/constants.js
+++ src/constants.js
@@ -39,5 +39,5 @@ export const INPUT_MESSAGES = {
 export const OUTPUT_MESSAGES = {
   PURCHASE_COUNT: (count) => `\n${count}개를 구매했습니다.`,
   STATISTICS_HEADER: "\n당첨 통계\n---",
-  PROFIT_RATE: (rate) => `총 수익률은 ${rate}%입니다.`,
+  PROFIT_RATE: (rate) => `총 수익률은 ${rate.toFixed(1)}%입니다.`,
 };
